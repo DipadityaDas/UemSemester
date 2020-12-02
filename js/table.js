a6dc5f84c81bf429327fb2ac54264a55804330bf
Dipaditya Das <DipadityaDas@gmail.com>
2020-12-02 21:15:43 +0530

    Theory Exams Update

--- js/table.js
+++ js/table.js
@@ -1,12 +1,19 @@
 var dataset = [
-	["06-12-2020", "9.30 am - 5 pm",     "Industrial Training Evaluation",            "CS782",  "Practical"],
-	["06-12-2020", "9.30 am - 5 pm",     "Project - I",					              "CS783",  "Practical"],
-	["06-12-2020", "9.30 am - 5 pm",     "Group Discussion",			              "HU781",  "Practical"],
-	["08-12-2020", "9.30 am - 12.30 pm", "Artificial Intelligence Laboratory",        "CS791",  "Practical"],
-	["08-12-2020", "2 pm - 5 pm",        "Compiler Design Laboratory",                "CS793",  "Practical"],
-	["09-12-2020", "9.30 am - 12.30 pm", "Skill Development for Professionals – VII", "GS781",  "Practical"],
-	["12-12-2020", "9.30 am - 12.30 pm", "Image Processing Laboratory",               "CS794D",	"Practical"],
-	["12-12-2020", "2 pm - 5 pm",        "Internet Technology Laboratory",            "CS795D", "Practical"]
+	["06-12-2020", "9.30 am - 5 pm", "Industrial Training Evaluation", "CS782", "Practical"],
+	["06-12-2020", "9.30 am - 5 pm", "Project - I", "CS783", "Practical"],
+	["06-12-2020", "9.30 am - 5 pm", "Group Discussion", "HU781", "Practical"],
+	["08-12-2020", "9.30 am - 12.30 pm", "Artificial Intelligence Laboratory", "CS791", "Practical"],
+	["08-12-2020", "2 pm - 5 pm", "Compiler Design Laboratory", "CS793", "Practical"],
+	["09-12-2020", "9.30 am - 12.30 pm", "Skill Development for Professionals – VII", "GS781", "Practical"],
+	["12-12-2020", "9.30 am - 12.30 pm", "Image Processing Laboratory", "CS794D", "Practical"],
+
+	["14-12-2020", "4 pm - 7 pm", "Artificial Intelligence Laboratory", "CS701", "Theory"],
+	["16-12-2020", "4 pm - 7 pm", "Cryptography & Network Security", "CS702", "Theory"],
+	["18-12-2020", "4 pm - 7 pm", "Essential Studies for Professionals - VII", "GS701", "Theory"],
+	["21-12-2020", "4 pm - 7 pm", "Compiler Design", "CS703", "Theory"],
+	["23-12-2020", "4 pm - 7 pm", "Image Processing", "CS704D", "Theory"],
+	["28-12-2020", "4 pm - 7 pm", "Internet Technology", "CS705D", "Theory"],
+	["30-12-2020", "4 pm - 7 pm", "Mobile Computing", "CS706A", "Theory"]
 ]
 
 $('#myTable').DataTable({
