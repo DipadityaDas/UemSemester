152e653293d47e2ad977ee4e5dc57d1f2b18c364
Dipaditya Das <DipadityaDas@gmail.com>
2020-12-03 13:41:26 +0530

    Search Bar and Toggle Buttons Added

--- js/table.js
+++ js/table.js
@@ -6,7 +6,6 @@ var dataset = [
 	["08-12-2020", "2 pm - 5 pm", "Compiler Design Laboratory", "CS793", "Practical"],
 	["09-12-2020", "9.30 am - 12.30 pm", "Skill Development for Professionals – VII", "GS781", "Practical"],
 	["12-12-2020", "9.30 am - 12.30 pm", "Image Processing Laboratory", "CS794D", "Practical"],
-
 	["14-12-2020", "4 pm - 7 pm", "Artificial Intelligence", "CS701", "Theory"],
 	["16-12-2020", "4 pm - 7 pm", "Cryptography & Network Security", "CS702", "Theory"],
 	["18-12-2020", "4 pm - 7 pm", "Essential Studies for Professionals - VII", "GS701", "Theory"],
@@ -30,8 +29,27 @@ $('#myTable').DataTable({
 			display: $.fn.dataTable.Responsive.display.childRowImmediate
 		}
 	},
+	columnDefs: [{
+		className: "dt-center",
+		targets: "_all"
+	}],
 	searching: true,
 	ordering: false,
 	paging: false,
 	info: false,
-})
+	dom: 'lrtip',
+});
+
+$('#searchBox').on('keyup', function () {
+	$('#myTable').DataTable().search(this.value).draw();
+});
+
+$('#practical').on('click', function () {
+	$('#myTable').DataTable().columns(4).search("Practical", true, false, true).draw();
+});
+
+$('#theory').on('click', function () {
+	$('#myTable').DataTable().columns(4).search("Theory", true, false, true).draw();
+});
+
+document.getElementById("practical").click();
