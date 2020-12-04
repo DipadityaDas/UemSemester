a6879722b0780f8c5c83e288d7789be5b9368344
Dipaditya Das <DipadityaDas@gmail.com>
2020-12-04 23:05:10 +0530

    Revised Time Table

--- js/table.js
+++ js/table.js
@@ -7,13 +7,14 @@ var dataset = [
 	["09-12-2020", "9.30 am - 12.30 pm", "Skill Development for Professionals – VII", "GS781", "Practical"],
 	["12-12-2020", "9.30 am - 12.30 pm", "Image Processing Laboratory", "CS794D", "Practical"],
 	["12-12-2020", "2 pm - 5 pm", "Internet Technology Laboratory", "CS795D", "Practical"],
+	
 	["14-12-2020", "4 pm - 7 pm", "Artificial Intelligence", "CS701", "Theory"],
-	["16-12-2020", "4 pm - 7 pm", "Cryptography & Network Security", "CS702", "Theory"],
-	["18-12-2020", "4 pm - 7 pm", "Essential Studies for Professionals - VII", "GS701", "Theory"],
-	["21-12-2020", "4 pm - 7 pm", "Compiler Design", "CS703", "Theory"],
-	["23-12-2020", "4 pm - 7 pm", "Image Processing", "CS704D", "Theory"],
-	["28-12-2020", "4 pm - 7 pm", "Internet Technology", "CS705D", "Theory"],
-	["30-12-2020", "4 pm - 7 pm", "Mobile Computing", "CS706A", "Theory"]
+	["15-12-2020", "4 pm - 7 pm", "Cryptography & Network Security", "CS702", "Theory"],
+	["16-12-2020", "4 pm - 7 pm", "Compiler Design", "CS703", "Theory"],
+	["21-12-2020", "4 pm - 7 pm", "Image Processing", "CS704D", "Theory"],
+	["22-12-2020", "4 pm - 7 pm", "Internet Technology", "CS705D", "Theory"],
+	["23-12-2020", "4 pm - 7 pm", "Essential Studies for Professionals - VII", "GS701", "Theory"],
+	["24-12-2020", "4 pm - 7 pm", "Mobile Computing", "CS706A", "Theory"]
 ]
 
 $('#myTable').DataTable({
